afd0ad849dd49fdd4eca5f22f1023c3da719d564
zerotohero200222 <aebharath17@gmail.com>
2026-03-25 22:39:44 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.5.0"
+  
+  backend "gcs" {
+    bucket = "eternal-bruin-489005-u2-tf-state"
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
