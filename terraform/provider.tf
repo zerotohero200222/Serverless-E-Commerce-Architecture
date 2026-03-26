6a8301b1e81614e4168f5a0cadf72105749de3c7
zerotohero200222 <aebharath17@gmail.com>
2026-03-26 10:43:57 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
   
   backend "gcs" {
-    bucket = "eternal-bruin-489005-u2-tf-state"
+    bucket = "eternal-bruin-489005-u2-tfstate"
     prefix = "terraform/state"
   }
 
