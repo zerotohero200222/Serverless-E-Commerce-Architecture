3d86e2cc61f9096ceca02f6fea999b59a2733a6a
zerotohero200222 <aebharath17@gmail.com>
2026-03-26 10:44:42 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "api_gateway_url" {
   description = "The default hostname of the managed API Gateway"
   value       = google_api_gateway_gateway.ecommerce_gateway.default_hostname
 }
-
-output "api_key_id" {
-  description = "The ID of the generated API Key for gateway access"
-  value       = google_apikeys_key.gateway_key.id
-}
