6c5fbfb5db4dc670fdb6181a94145b2dbcb7f9aa
zerotohero200222 <aebharath17@gmail.com>
2026-04-19 22:45:30 +0530

    Delete terraform directory

--- terraform/environments/prod.tfvars
+++
@@ -1,2 +0,0 @@
-project_id = "eternal-bruin-489005-u2"
-region     = "us-central1"
--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "load_balancer_ip" {
-  description = "The global IP address of the External Load Balancer"
-  value       = google_compute_global_address.ecommerce_ip.address
-}
-
-output "api_gateway_url" {
-  description = "The default hostname of the managed API Gateway"
-  value       = google_api_gateway_gateway.ecommerce_gateway.default_hostname
-}
